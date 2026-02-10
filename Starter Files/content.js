07efac2808802b0fd95b64efea84535744b964a2
Pradeep4157 <krp18539@gmail.com>
2026-02-10 21:59:03 +0530

    added the icon on the page..

--- Starter Files/content.js
+++ Starter Files/content.js
@@ -1,7 +1,24 @@
-// content.js
-// Author:
-// Author URI: https://
-// Author Github URI: https://www.github.com/
-// Project Repository URI: https://github.com/
-// Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
-// License: MIT
+const bookMarkImageURL = chrome.runtime.getURL('assets/bookmark.png');
+
+
+function addBookMarkButton(){
+    const question = document.querySelector(".coding_problem_info_heading__G9ueL");
+    if(!question) return;
+    if(question.querySelector('#add-bookmark-button')) return;
+    const bookMarkButton = document.createElement('img');
+    bookMarkButton.id = 'add-bookmark-button';
+    bookMarkButton.src = bookMarkImageURL;
+    bookMarkButton.style.marginLeft = '8px';
+    bookMarkButton.style.height='30px';
+    bookMarkButton.style.width = '30px';
+    bookMarkButton.style.cursor = 'pointer';
+    question.appendChild(bookMarkButton);
+}
+
+
+addBookMarkButton();
+const observer = new MutationObserver(addBookMarkButton);
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
